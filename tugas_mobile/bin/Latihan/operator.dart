6fe1547a58d960ad147500c214c91418e56c509e
maya-alr <nurmayabahrudin@gmail.com>
2025-09-28 17:06:34 -0700

    Variabel dan tipe data

---
+++ tugas_mobile/bin/Latihan/operator.dart
@@ -0,0 +1,37 @@
+// main() {
+//   int a = 5;
+//   int b = 10;
+
+//   print("a + b : ${a +  b}"); //$ format interpolasion
+//   print("a - b : ${a - b}");
+//   print("a * b : ${a *  b}");
+//   print("a / b : ${a / b}");
+//   print("a % b : ${a %  b}");
+//   print("a !/ b : ${a !/  b}"); 
+//   print("a >= b : ${a >=  b}");
+  
+
+
+// }
+
+// main () {
+//   String username = "nurmaya bahrudin";
+//   int  password = 1234;
+
+//   if (username = "admin" &&  password = '123');
+//   print("login berhasil");
+//   else {
+
+//   }
+// }
+
+import dart.io
+
+main() {
+  
+  stdout.write("Masukkan Nama :");
+
+  String? nama = stdin.readLineSync();
+  
+  print("nama saya : $nama");
+}
